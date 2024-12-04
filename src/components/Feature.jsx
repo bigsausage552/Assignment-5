2c8ea6c597dc4682e50dcdb3cdde72a78a2300b1
David Liu <david.liu4@student.tdsb.on.ca>
2024-12-04 11:30:27 -0500

    Added changes to css

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -5,11 +5,7 @@ import "./Feature.css";
 
 function Feature() {
   const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
-  const [topRatedMovies, setTopRatedMovies] = useState([]);
   const navigate = useNavigate();
-  const movieListRef = useRef([]);
-  const clickCounts = useRef({});
-
   useEffect(() => {
     const fetchMovies = async () => {
       const nowPlayingResponse = await axios.get(
@@ -17,9 +13,6 @@ function Feature() {
       );
       setNowPlayingMovies(nowPlayingResponse.data.results.slice(0, 4));
 
-      const topRatedResponse = await axios.get(
-        `https://api.themoviedb.org/3/movie/top_rated?api_key=${import.meta.env.VITE_TMDB_KEY}`
-      );
       const moviesData = topRatedResponse.data.results;
       const randomMovies = [];
       let availableMovies = [...moviesData];
@@ -34,31 +27,6 @@ function Feature() {
 
     fetchMovies();
   }, []);
-
-  const arrowClick = (index) => {
-    const listElement = movieListRef.current[index];
-    const items = listElement.querySelectorAll(".movie-list-item");
-    const itemCount = items.length;
-    const ratio = Math.floor(window.innerWidth / 270);
-
-    if (!clickCounts.current[index]) {
-      clickCounts.current[index] = 0;
-    }
-
-    const maxClicks = itemCount - (4 + clickCounts.current[index]) + (3 - ratio);
-    clickCounts.current[index]++;
-
-    if (maxClicks >= 0) {
-      const transformValue =
-        parseFloat(
-          getComputedStyle(listElement).transform.split(",")[4] || 0
-        ) - 277;
-      listElement.style.transform = `translateX(${transformValue}px)`;
-    } else {
-      listElement.style.transform = "translateX(0)";
-      clickCounts.current[index] = 0;
-    }
-  };
 
   return (
     <div className="feature-container">
@@ -96,41 +64,6 @@ function Feature() {
               </div>
             ))}
           </div>
-        </div>
-      </div>
-
-      <div className="movie-list-container">
-        <h1 className="movie-list-title">Top Rated</h1>
-        <div className="movie-list-wrapper">
-          <div
-            className="movie-list"
-            ref={(el) => (movieListRef.current[0] = el)}
-          >
-            {topRatedMovies.map((movie) => (
-              <div
-                key={movie.id}
-                className="movie-list-item"
-                onClick={() => navigate(`/movies/details/${movie.id}`)}
-              >
-                <img
-                  className="movie-list-item-img"
-                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                  alt={movie.title}
-                />
-                <span className="movie-list-item-title">{movie.title}</span>
-                <p className="movie-list-item-description">
-                  {movie.overview.length > 150
-                    ? movie.overview.substring(0, 150) + "..."
-                    : movie.overview}
-                </p>
-                <button className="movie-list-item-button">Watch Now</button>
-              </div>
-            ))}
-          </div>
-          <i
-            className="fa-solid fa-chevron-right arrow"
-            onClick={() => arrowClick(0)}
-          ></i>
         </div>
       </div>
     </div>
